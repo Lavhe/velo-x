de1d337b9c246961d43c8afbcff58644b7fc870a
Joe <mulavhe@gmail.com>
2023-11-08 09:12:08 +0200

    :rocket: Fixing builds

--- libs/theme/tailwind.config.js
+++ libs/theme/tailwind.config.js
@@ -1,23 +1,13 @@
-const plugin = require('tailwindcss/plugin');
-
 module.exports = {
-  darkMode: 'class',
-  content: ['**/*.ts', '**/*.tsx'],
+  darkMode: "class",
+  content: ["**/*.ts", "**/*.tsx"],
   theme: {
     extend: {
       colors: {
-        primary: '#1fcecb',
-        secondary: '#818285'
-      }
-    }
+        primary: "#1fcecb",
+        secondary: "#818285",
+      },
+    },
   },
-  plugins: [
-    plugin(({ addUtilities }) => {
-      addUtilities({
-        // 😎 similar to `@apply`
-        Text: `px-4 py-1 rounded-full bg-red-800 text-white`,
-        '.body-text': `font-serif leading-relaxed tracking-wide text-gray-800`
-      });
-    })
-  ]
+  plugins: [],
 };
